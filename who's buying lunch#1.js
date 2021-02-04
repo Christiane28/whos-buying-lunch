81dd98faa23060951f04363329d1d6738e7ee0bc
Christiane28 <65940044+Christiane28@users.noreply.github.com>
2021-02-04 16:58:54 +1100

    Add files via upload

---
+++ who's buying lunch#1.js
@@ -0,0 +1,11 @@
+var names = ["Cate", "Christiane", "Jake"];
+
+function whosPaying(names) {
+
+  var numberOfPeople = names.length;
+  var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+  var randomPerson = names[randomPersonPosition];
+
+  return randomPerson + " " + "is going to buy lunch today!";
+
+}
---
+++ who's buying lunch#2.js
@@ -0,0 +1,7 @@
+function whosPaying(names) {
+
+  var names = ["Cate", "Christiane", "Jake"];
+  var randomName = names[Math.floor(Math.random() * names.length)];
+
+  return randomName + " " + "is going to buy lunch today!";
+}
